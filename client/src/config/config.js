4eb2eddd98f790ac102a8d3192da04053ac2d8ee
mxnuchim <manuchimoliver779@gmail.com>
2023-04-01 18:13:58 +0100

    fix: frontend deployment fix

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: 'http://https://project-threejs-ai-ukx8.onrender.com/api/v1',
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: 'https://devswag.onrender.com/api/v1',
   },
 };
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -57,15 +57,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        'https://project-threejs-ai-ukx8.onrender.com/api/v1',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
