b87bac1fe01030b24d06232733fad68ae5b5c0d7
mxnuchim <manuchimoliver779@gmail.com>
2023-04-01 16:26:21 +0100

    fix: cors fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import routes from './routes/dalle.routes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1', routes);
